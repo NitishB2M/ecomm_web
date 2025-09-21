75aa66c6cd58f0b40cbd14174495f8608d7c3234
NitishB2M <78561376+Nitish-B2M@users.noreply.github.com>
2025-09-22 00:02:43 +0530

    Add email verification and update route constants
    Introduces an email verification page and related hook logic, updates API endpoints and route constants for better role and route management, and improves whitelist path matching. Removes unused customTypography component and refactors registration logic in useProfile. Minor Tailwind and CSS utility updates included.

--- src/utils/APICalls.js
+++ src/utils/APICalls.js
@@ -1,5 +1,7 @@
 "use client";
 import { GenerateHash, CURRENT_USER, handleLogout, GET_AUTH_TOKEN } from "./Helpers";
+
+const statusCode = [400, 401, 403, 404, 500, 502, 503, 504];
 const ApiCall = async ({ url, method = "GET", body, withToken = true, header = {} }) => {
   try {
     const { token, isValid } = GET_AUTH_TOKEN();
@@ -18,25 +20,29 @@ const ApiCall = async ({ url, method = "GET", body, withToken = true, header = {
       headers: {
         "Content-Type": "application/json",
         "Authorization": withToken && token && isValid ? `Bearer ${token}` : null,
-        // ...header,
+        ...header,
       },
       body: JSON.stringify(body),
     });
 
+    const jsonResponse = await res.json();
     if (res.ok) {
-      return await res.json();
+      return jsonResponse;
     } else {
       if (res.status === 401) {
         eventEmitter.emit('unauthorized', {});
         handleLogout();
         return;
       }
-      if (res.status === 409) {
-        res = await res.json();
-        throw { ...res, statusCode: 409 };
+      
+      if (statusCode.includes(res.status) || res.status === 400) {
+        throw { ...jsonResponse, statusCode: res.status };
       }
     }
   } catch (error) {
+    if (error.statusCode || error.status !== undefined) {
+      throw error;
+    }
     throw new Error("Failed to fetch");
   }
 };
